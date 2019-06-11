8e66a11e021bed491077735ddc7f23f72f05933b
jessepatricio.gmail.com <jessepatricio@gmail.com>
2019-06-11 16:06:05 +1200

    add searchbar component

--- figApp/src/components/App.js
+++ figApp/src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import Header from './Header';
+import SearchBar from './SearchBar';
 
 
 
 export class App extends Component {
     render() {
         return (
-            <div>
+            <div className="ui container"> 
                 <Header title="Acme Contact Management"/>
+                <SearchBar />
             </div>
         )
     }
---
+++ figApp/src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class SearchBar extends Component {
+    state = { srcText: '' };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(this.state.srcText);
+    }
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">  
+                    <label labelfor="searchbox">Search Contacts: </label>
+                    <input id="searchbox" 
+                        type="text"
+                        value={this.state.srcText} 
+                        onChange={e => this.setState({ srcText: e.target.value })}/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar
